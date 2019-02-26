7dce77d8d91d7a619b874fccd981a9d0006d609d
jacobfelknor <47924156+jacobfelknor@users.noreply.github.com>
2019-02-26 10:57:27 -0700

    Update twitterStats2019.py

--- twitterStats2019.py
+++ twitterStats2019.py
@@ -1,10 +1,11 @@
 # Import the tweepy library
 import tweepy
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = '3067050784-dHA28dy0Q5dRQ6gq0FbtP1RSOIiUTUaqbGorU3e'
-ACCESS_SECRET = 'prXOwddr07Pz5kcuwZYTOxKGWRkbjylaqsr022EvhkHXu'
-CONSUMER_KEY = 'mjjwlnKgURc4IkQ4bZ55GlVJp'
-CONSUMER_SECRET = '2eDxNQ1AjRc7lEe2XbgvW1DJ6BZjaoojiStIdEGYhtqmcoF6hL'
+# Must have a developer twitter account to obtain these keys
+ACCESS_TOKEN = 'access token here'
+ACCESS_SECRET = 'acccess secret here'
+CONSUMER_KEY = 'consumer key here'
+CONSUMER_SECRET = 'consumer secret here'
 
 
 def userInfo(user_name, api):
